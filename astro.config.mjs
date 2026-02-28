98778002ff3861ab2de673bb3bd6bff2d1ba622d
junior-boop <juniorseppo3@gmail.com>
2026-02-28 16:13:24 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react()],
+  adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
